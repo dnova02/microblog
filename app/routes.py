91ac1dcfb7178349a26f738600cacf1e121922c3
dnova02 <zahsko.og@gmail.com>
2023-04-01 10:52:36 -0400

    init

--- app/routes.py
+++ app/routes.py
@@ -1,51 +1,41 @@
-from app import app
-
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Dariel'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
-
-from app import app
-
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Miguel'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
-
-
 from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': "Dariel"}
+    user = {'username': 'Dariel'}
     posts = [
         {
-             'author': {'username': 'Brian'},
-             'body': 'Beautiful day in Washington!'
+            'author': {'username': 'zahsko'},
+            'body': "Kenneth can't type for shit"
         },
         {
-             'author': {'username': 'Makayla'},
-             'body': 'The Last of Us show is so cool!'
+            'author': {'username': 'ovhgqd'},
+            'body': "Dariel can't beat me in a 1v1"
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+from flask import render_template
+from app import app
+from app.forms import LoginForm
+
+# ..
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Sign In", form=form)
+
+from flask import render_template, flash, redirect
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("Login requested for user {}, remember_me{}" .format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title="Sign In", form=form)
