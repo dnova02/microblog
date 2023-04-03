a03ccd91a2fbbdc57b104ecdee3bcd4e11afaf01
dnova02 <zahsko.og@gmail.com>
2023-04-03 19:22:40 -0400

    finished chapter 4

--- app/routes.py
+++ app/routes.py
@@ -7,12 +7,12 @@ def index():
     user = {'username': 'Dariel'}
     posts = [
         {
-            'author': {'username': 'zahsko'},
-            'body': "Kenneth can't type for shit"
+            'author': {'username': 'Brian'},
+            'body': "I'm obsessed with Ready Or Not"
         },
         {
-            'author': {'username': 'ovhgqd'},
-            'body': "Dariel can't beat me in a 1v1"
+            'author': {'username': 'Makayla'},
+            'body': "I play Sims 4"
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
@@ -24,18 +24,13 @@ from app.forms import LoginForm
 
 # ..
 
-@app.route('/login')
-def login():
-    form = LoginForm()
-    return render_template("login.html", title="Sign In", form=form)
-
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash("Login requested for user {}, remember_me{}" .format(
+        flash("Login requested for user {}, remember_me{}".format(
             form.username.data, form.remember_me.data))
         return redirect('/index')
     return render_template('login.html', title="Sign In", form=form)
