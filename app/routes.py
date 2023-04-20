c48c1f091a1f180d369b8243e282aa9582abb795
dnova02 <zahsko.og@gmail.com>
2023-04-19 23:20:01 -0400

    implemented password hashes using Werkzeug and installed Flask-Login

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,11 @@
 from flask import render_template
 from app import app
+from flask_login import current_user, login_user
+from flask import render_template
+from app import app
+from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -17,19 +23,16 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-
-from flask import render_template
-from app import app
-from app.forms import LoginForm
-
-# ..
-
-from flask import render_template, flash, redirect, url_for
-
 @app.route('/login', methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_username(form.username.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
         flash("Login requested for user {}, remember_me{}".format(
             form.username.data, form.remember_me.data))
         return redirect('/index')
