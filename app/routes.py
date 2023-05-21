32d40fae5cff2ff26edf4a325cc703e41642f6d4
dnova02 <zahsko.og@gmail.com>
2023-05-21 13:37:50 -0400

    added explore tab

--- app/routes.py
+++ app/routes.py
@@ -13,12 +13,20 @@ from app.forms import RegistrationForm
 from datetime import datetime
 from app.forms import EditProfileForm
 from app.forms import EmptyForm
+from app.forms import PostForm
+from app.models import Post
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    user = {'username': 'Dariel'}
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
     posts = [
         {
             'author': {'username': 'Brian'},
@@ -29,7 +37,8 @@ def index():
             'body': "I play Sims 4"
         }
     ]
-    return render_template('index.html', title='Home Page', posts=posts)
+    posts = current_user.followed_posts().all()
+    return render_template('index.html', title='Home Page', form=form, posts=posts)
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
@@ -139,3 +148,9 @@ def unfollow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
+
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
